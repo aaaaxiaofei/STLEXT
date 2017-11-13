3846954b56533f8d0a211570d1d9c3936ebf3b58
Fei <aaaaxiaofei@163.com>
2017-11-13 11:56:38 -0500

    add strCmp() function

--- function.hpp
+++ function.hpp
@@ -31,3 +31,19 @@ void round_robin(deque<Job>& job_queue, deque<Job>& done_queue, int quantum, int
 	}
 
 }
+
+int strCmp(char string1[], char string2[] )
+{
+    for (int i = 0; ; i++)
+    {
+        if (string1[i] != string2[i])
+        {
+            return string1[i] < string2[i] ? -1 : 1;
+        }
+
+        if (string1[i] == '\0')
+        {
+            return 0;
+        }
+    }
+}
