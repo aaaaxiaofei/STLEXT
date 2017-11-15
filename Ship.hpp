862a4e8f94e24d1d1766e73a389c08dc52b8d5fb
Fei <aaaaxiaofei@163.com>
2017-11-15 00:16:29 -0500

    Ship class

---
+++ Ship.hpp
@@ -0,0 +1,24 @@
+#ifndef SHIP_HPP
+#define SHIP_HPP
+
+class Ship{
+
+private:
+	int x;
+	int y;
+	int len;
+	bool flag_vertical;
+
+public:
+	Ship() {}
+	Ship(int xx, int yy, int l, bool vertical) {
+
+	}
+	int X() {return x;}
+	int Y() {return y;}
+	int length() {return len;}
+	bool isVertical() {return flag_vertical;}
+};
+
+
+#endif
